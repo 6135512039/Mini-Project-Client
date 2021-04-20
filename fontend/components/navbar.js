d6c643f26b1de49b158135b22daa0ddf9c9e5d0e
6135512039 <Kongkom25@gmail.com>
2021-04-20 13:50:32 +0700

    fourth commit

--- fontend/components/navbar.js
+++ fontend/components/navbar.js
@@ -1,8 +1,9 @@
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import styles from '../styles/components.css'
 
 const Navbar = () => (
-    <div className={styles.center}>
+    /*<div className={styles.center}>
         <h1  className={styles.header}>Cars Information</h1>
         <Link href="/"><a> Home </a></Link> |
         <Link href="/register"><a> Register </a></Link>  |
@@ -12,7 +13,17 @@ const Navbar = () => (
         <Link href="/editCars"><a> Cars </a></Link> |
         <Link href="/logout"><a> Logout </a></Link> 
         
-    </div>
+    </div>*/
+
+<div className={styles.nav-items}>
+<Link href="/"><a> Home </a></Link> |
+        <Link href="/register"><a> Register </a></Link>  |
+        <Link href="/login"><a> Login </a></Link> |
+        <Link href="/getConfig"><a> Config </a></Link> |       
+        <Link href="/profile"><a> Profile </a></Link> | 
+        <Link href="/editCars"><a> Cars </a></Link> |
+        <Link href="/logout"><a> Logout </a></Link> 
+</div>
 )
 
 export default Navbar
